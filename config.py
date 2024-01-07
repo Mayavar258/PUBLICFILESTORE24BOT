ad509a521f0d3a8387e81b6c89fa4bc973f745d2
Mayavar258 <123068420+Mayavar258@users.noreply.github.com>
2024-01-07 15:45:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,21 +24,21 @@ def is_enabled(value, default):
 # Don't Remove Credit Tg - @Sunrises_24
       
 # Owner Information
-API_ID = int(environ.get("API_ID", " "))
-API_HASH = environ.get("API_HASH", " ")
-ADMINS = int(environ.get("ADMINS", " "))
+API_ID = int(environ.get("API_ID", "1522127"))
+API_HASH = environ.get("API_HASH", "1252ffe16baf341bfd7236f92df76b0e")
+ADMINS = int(environ.get("ADMINS", "1006159057"))
 
 # Database Information
-CLONE_DB_URI = environ.get("CLONE_DB_URI", " ")
-CDB_NAME = environ.get("CDB_NAME", "Sunrises24Bots")
-DB_URI = environ.get("DB_URI", " ")
-DB_NAME = environ.get("DB_NAME", "Harsha24Bots")
+CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://good:good@mayan.zejzhhz.mongodb.net/?retryWrites=true&w=majority")
+CDB_NAME = environ.get("CDB_NAME", "maya")
+DB_URI = environ.get("DB_URI", "mongodb+srv://mayan:mayan@mayavar.4ujjgia.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = environ.get("DB_NAME", "mayavar")
 
 # Don't Remove Credit Tg - @Sunrises_24
 
 # Bot Information
-BOT_TOKEN = environ.get("BOT_TOKEN", " ")
-BOT_USERNAME = environ.get("BOT_USERNAME", " ") # your bot username without @
+BOT_TOKEN = environ.get("BOT_TOKEN", "6294042089:AAGRVfK3-scO5DLufuKBcuaCltk9Tb8tUvI")
+BOT_USERNAME = environ.get("BOT_USERNAME", "MN_Filestorebot") # your bot username without @
 PICS = (environ.get('PICS', 'https://graph.org/file/8632772ac4e8f4ac7981a.jpg')).split() # Bot Start Picture
 
 # Auto Delete Information
@@ -46,8 +46,8 @@ AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
 AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
 
 # Channel Information
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", " "))
-FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).split()]
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002125151670"))
+FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1002126415310')).split()]
 
 # Don't Remove Credit Tg - @Sunrises_24
 
